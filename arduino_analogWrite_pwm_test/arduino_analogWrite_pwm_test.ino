8a3f9dd19ce85d74aa24eae34c6ef6cf716cc0cf
Walter Spurgiasz <walter@conciseusa.com>
2023-09-04 19:46:35 -0700

    Fix bug where brightness would overrun when using button control

--- arduino_analogWrite_pwm_test/arduino_analogWrite_pwm_test.ino
+++ arduino_analogWrite_pwm_test/arduino_analogWrite_pwm_test.ino
@@ -1,5 +1,5 @@
 
-#define BUTTON 7     // the input pin the the optional control button is attached to
+#define BUTTON 7     // the input pin the optional control button is attached to
 #define AUTO_CYCLE_PIN 8  // pull low to use button to set brightness
 #define PWM 9        // the PWM pin the LED is attached to
 int brightness = 5;  // how bright the LED is, start off dim
@@ -15,7 +15,8 @@ void setup() {
   // #define SERIALP Serial1
   // examples of auto setting SERIALP: https://github.com/conciseusa/arduino-rtc-json-data-logger
 
-  // Open serial communications and wait for port to open:
+  // Open serial communications and wait for port to open, uncomment to use.
+/*
 #if defined(USE_SERIAL1)
   Serial1.begin(9600);
   while (!Serial1) {
@@ -27,13 +28,14 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 #endif
+*/
 
   pinMode(BUTTON, INPUT_PULLUP);
   pinMode(AUTO_CYCLE_PIN, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT); // initialize digital pin LED_BUILTIN as an output.
   pinMode(PWM, OUTPUT); // declare pwm pin set above to be an output.
 
-  // test some extremes at start up
+  // test some extremes at start up, comment out if not wanted.
   analogWrite(PWM, 255);
   delay(2000);
   analogWrite(PWM, 0);
@@ -44,7 +46,14 @@ void setup() {
 
 void loop() { // the loop function runs over and over again forever
   if ((digitalRead(AUTO_CYCLE_PIN) == LOW) && (digitalRead(BUTTON) == LOW) && (digitalRead(BUTTON) != prev_button_state)) {
-    fadeAmount = -fadeAmount; // if button was just pressed, toggle fade
+    // if button was just pressed, toggle fade, but point inward if at extremes
+    if (brightness <= 0) {
+      fadeAmount = 1;
+    } else if (brightness >= 255) {
+      fadeAmount = -1;
+    } else {
+      fadeAmount = -fadeAmount;
+    }
   }
   prev_button_state = digitalRead(BUTTON);
 
@@ -54,7 +63,7 @@ void loop() { // the loop function runs over and over again forever
     digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   }
 
-  if ((digitalRead(AUTO_CYCLE_PIN) == HIGH) || (digitalRead(BUTTON) == LOW)) {
+  if ((digitalRead(AUTO_CYCLE_PIN) == HIGH) || (digitalRead(BUTTON) == LOW)) { // if in auto mode or button pressed
     brightness = brightness + fadeAmount; // change the brightness to ramp up and down
   }
 
@@ -73,7 +82,7 @@ void loop() { // the loop function runs over and over again forever
 
   // wait X milliseconds to see the dimming effect
   if (brightness < 5) {
-    delay(fadeDelay * 4); // slow down at the very end
+    delay(fadeDelay * 4); // slow down at the very dim end
   } else {
     delay(fadeDelay);
   }
